41707f84f98362f8421604de8ca815e4bfb6bb51
BrianJomo <brian.jomo@student.moringaschool.com>
2022-04-19 20:20:12 +0300

    Add files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Git'`, () => {
+  it(`should have as title 'Github-search'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Git');
+    expect(app.title).toEqual('Github-search');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Git app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Github-search app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+import { UserReposComponent } from './user-repos/user-repos.component';
+import { MyModuleModule } from './my-module/my-module.module';
+import { FooterComponent } from './footer/footer.component';
+import { DirectiveDirective } from './directive.directive';
+import { MypipePipe } from './mypipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    SearchUserComponent,
+    AboutComponent,
+    UserReposComponent,
+    FooterComponent,
+    DirectiveDirective,
+    MypipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MyModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
